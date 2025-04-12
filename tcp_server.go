bacc84e5daa021673954116e5dbcf334f39ff62f
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-04-12 18:46:50 +0500

    feat: :sparkles: Add the `TCPServer` structure

---
+++ tcp_server.go
@@ -0,0 +1,96 @@
+package tcpServer
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sync"
+	"sync/atomic"
+)
+
+const (
+	TCPServerNetwork = "tcp"
+)
+
+type ConnectionHandler interface {
+	HandleConnection(ctx context.Context, connection net.Conn) error
+}
+
+type ErrorHandler func(err error)
+
+type TCPServerOptions struct {
+	Address           string
+	ConnectionHandler ConnectionHandler
+	ErrorHandler      ErrorHandler
+}
+
+type TCPServer struct {
+	options   TCPServerOptions
+	listener  net.Listener
+	isStopped atomic.Bool
+}
+
+func NewTCPServer(
+	ctx context.Context,
+	options TCPServerOptions,
+) (*TCPServer, error) {
+	var listenConfig net.ListenConfig
+	listener, err := listenConfig.Listen(ctx, TCPServerNetwork, options.Address)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"unable to listen on address %q: %w",
+			options.Address,
+			err,
+		)
+	}
+
+	server := &TCPServer{
+		options:  options,
+		listener: listener,
+	}
+	return server, nil
+}
+
+func (server *TCPServer) Address() string {
+	return server.listener.Addr().String()
+}
+
+func (server *TCPServer) Run(ctx context.Context) {
+	var waitGroup sync.WaitGroup
+	ctx, ctxCancel := context.WithCancel(ctx)
+	for !server.isStopped.Load() {
+		connection, err := server.listener.Accept()
+		if err != nil {
+			server.options.ErrorHandler(fmt.Errorf(
+				"unable to accept the connection: %w",
+				err,
+			))
+			continue
+		}
+
+		waitGroup.Add(1)
+
+		go func() {
+			defer waitGroup.Done()
+			defer connection.Close()
+
+			if err := server.options.ConnectionHandler.HandleConnection(
+				ctx,
+				connection,
+			); err != nil {
+				server.options.ErrorHandler(fmt.Errorf(
+					"unable to handle the connection: %w",
+					err,
+				))
+			}
+		}()
+	}
+
+	ctxCancel()
+	waitGroup.Wait()
+}
+
+func (server *TCPServer) Stop() {
+	server.isStopped.Store(true)
+	server.listener.Close()
+}
