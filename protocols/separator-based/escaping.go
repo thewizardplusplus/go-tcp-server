cc66bd9f775eb0c85ff42e0e817f6032bf0d3f56
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-04-17 01:56:27 +0500

    feat(protocols/separator-based): :sparkles: Add the `UnescapeSeparators()` function

--- protocols/separator-based/escaping.go
+++ protocols/separator-based/escaping.go
@@ -3,6 +3,7 @@ package separatorBasedTCPServerProtocol
 import (
 	"bytes"
 	"encoding/hex"
+	"fmt"
 )
 
 func EscapeSeparators(data []byte, separators [][]byte) []byte {
@@ -22,4 +23,36 @@ func EscapeSeparators(data []byte, separators [][]byte) []byte {
 	}
 
 	return data
+}
+
+func UnescapeSeparators(data []byte) ([]byte, error) {
+	unescapedData := make([]byte, 0, len(data))
+	for index := 0; index < len(data); {
+		if data[index] != '%' {
+			unescapedData = append(unescapedData, data[index])
+			index++
+
+			continue
+		}
+
+		if index+2 >= len(data) {
+			return nil, fmt.Errorf("not enough bytes to decode at position %d", index)
+		}
+
+		unescapedByte := make([]byte, 1)
+		escapedByte := data[index+1 : index+3]
+		if _, err := hex.Decode(unescapedByte, escapedByte); err != nil {
+			return nil, fmt.Errorf(
+				"unable to decode the byte sequence 0x%x at position %d: %w",
+				escapedByte,
+				index,
+				err,
+			)
+		}
+
+		unescapedData = append(unescapedData, unescapedByte...)
+		index += 3
+	}
+
+	return unescapedData, nil
 }
--- protocols/separator-based/escaping_test.go
+++ protocols/separator-based/escaping_test.go
@@ -57,3 +57,82 @@ func TestEscapeSeparators(test *testing.T) {
 		})
 	}
 }
+
+func TestUnescapeSeparators(test *testing.T) {
+	type args struct {
+		data []byte
+	}
+
+	for _, data := range []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "success/without separators",
+			args: args{
+				data: []byte("dummy"),
+			},
+			want:    []byte("dummy"),
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success/with a single separator type",
+			args: args{
+				data: []byte("one%3a%3dtwo;three%3a%3dfour;"),
+			},
+			want:    []byte("one:=two;three:=four;"),
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success/with several separator types",
+			args: args{
+				data: []byte(
+					"one%3a%3dtwo;three%3c%2dfour;five%3a%3dsix;seven%3c%2deight;",
+				),
+			},
+			want:    []byte("one:=two;three<-four;five:=six;seven<-eight;"),
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success/with the percent character",
+			args: args{
+				data: []byte("one%3a%3d23%25;two%3a%3d42%25;"),
+			},
+			want:    []byte("one:=23%;two:=42%;"),
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error/not enough characters to decode/zero characters",
+			args: args{
+				data: []byte("one%3a%3dtwo;three%3a%"),
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "error/not enough characters to decode/single character",
+			args: args{
+				data: []byte("one%3a%3dtwo;three%3a%3"),
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "error/unable to decode the sequence",
+			args: args{
+				data: []byte("one%3a%xxtwo;three%3a%3dfour;"),
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			got, err := UnescapeSeparators(data.args.data)
+
+			assert.Equal(test, data.want, got)
+			data.wantErr(test, err)
+		})
+	}
+}
